6cd95b4f860e256ad9f0deb2cc90d6f2148ee295
William Galindez Arias <william.dev.env@gmail.com>
2022-04-14 19:22:40 +0200

    Update test_inputs.py

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,1 +1,23 @@
-# Tests to check input data is not empty
+import pytest
+
+from fastapi.testclient import TestClient
+
+# Import our app from main.py.
+from app import app
+
+
+# Instantiate the testing client with our app.
+@pytest.fixture
+def client():
+    """
+    Get dataset
+    """
+    api_client = TestClient(app)
+    return api_client
+
+
+# Write tests using the same syntax as with the requests' module.
+def test_get(client):
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {'message': 'Welcome to the salary predictor API'}
