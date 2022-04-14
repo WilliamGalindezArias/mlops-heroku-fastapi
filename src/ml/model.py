a09c6d47eac3c147caa669a008f2e08046af6966
William Arias <warias@gitlab.com>
2022-04-14 02:34:38 +0200

    training done

---
+++ src/ml/model.py
@@ -0,0 +1,117 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier as rfc
+from .data import process_data
+import pandas as pd
+
+
+# Optional: implement hyperparameter tuning.
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model : RandomForestClassifier
+        Trained machine learning model.
+    """
+    clf = rfc()
+    clf.fit(X_train, y_train)
+
+    return clf
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    f_beta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, f_beta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+    Inputs
+    ------
+    model : RandomForestClassifier
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    predictions = model.predict(X)
+    return predictions
+
+
+def compute_metrics_by_slice(clf, encoder, lb, df, target, cat_columns, output_path=None):
+    """Compute metrics by slice of the data.
+    For simplicity, the function outputs the performance on slices of just the categorical features.
+    Inputs
+    ------
+    clf: Classifier model (scikit-learn compliant)
+    encoder: trained one-hot-encoder (output of the data.process_data function on training data)
+    lb: trained label binarizer (output of the data.process_data function on training data)
+    df: pandas dataframe where to compute metrics by slice
+    target: target column in the df input dataframe
+    cat_columns: categorical columns
+    output_path: output path to write the output dataframe.
+    Returns
+    -------
+    metrics_df: pd.DataFrame
+        Predictions by slice on the input data according the categorical columns.
+    """
+    columns = ["column", "category", "precision", "recall", "f1"]
+    metrics_df = pd.DataFrame(columns=columns)
+
+    for col in cat_columns:
+        for category in df[col].unique():
+
+            df_line = {}
+
+            tmp_df = df[df[col] == category]
+
+            X, y, _, _ = process_data(
+                X=tmp_df,
+                categorical_features=cat_columns,
+                label=target,
+                training=False,
+                encoder=encoder,
+                lb=lb
+            )
+
+            preds = inference(clf, X)
+
+            precision, recall, f1 = compute_model_metrics(y, preds)
+
+            df_line['column'] = col
+            df_line['category'] = category
+            df_line['precision'] = precision
+            df_line['recall'] = recall
+            df_line['f1'] = f1
+
+            metrics_df = metrics_df.append(df_line, ignore_index=True)
+
+    if output_path is not None:
+        metrics_df.to_csv(output_path)
+
+    return metrics_df
--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,74 @@
 # Model training 
+# Script to train machine learning model.
+
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml import model
+
+import pandas as pd
+import pickle as pkl
+
+
+data_path = "../data/census.csv"
+model_path = "../models/classifier.pkl"
+metrics_path = "../models/metrics_slice.csv"
+
+
+def save_model(model_path, encoder, lb, clf):
+    with open(model_path, "wb") as f:
+        pkl.dump([encoder, lb, clf], f)
+
+
+# 1. Load data
+data = pd.read_csv(data_path, index_col=0, skipinitialspace=True)
+
+# 2. Train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+# 3. Process data
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label='salary', training=True
+)
+
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
+# 4. Train and save a model
+
+clf = model.train_model(X_train, y_train)
+
+save_model(model_path=model_path, encoder=encoder, lb=lb, clf=clf)
+
+# 5. Inference
+
+training_predictions = model.inference(clf, X_train)
+testing_predictions = model.inference(clf, X_test)
+
+# 6. Metrics
+
+precision, recall, f_beta = model.compute_model_metrics(y_test, testing_predictions)
+
+# 7. Metrics by slice
+
+model.compute_metrics_by_slice(
+    clf=clf,
+    encoder=encoder,
+    lb=lb,
+    df=test,
+    target="salary",
+    cat_columns=cat_features,
+    output_path=metrics_path
+)
+
